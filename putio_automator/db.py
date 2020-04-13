828077e31cecd463653beb097b38fe04dcd7a807
datashaman <marlinf@datashaman.com>
2020-04-13 21:53:59 +0200

    migrate to click

--- putio_automator/db.py
+++ putio_automator/db.py
@@ -7,16 +7,16 @@ from putio_automator import APP_NAME, APP_AUTHOR
 
 database_path = os.path.join(appdirs.user_data_dir(APP_NAME, APP_AUTHOR), 'downloads.db')
 
-def with_db(app, func):
+def with_db(func):
     if not os.path.exists(database_path):
         open(database_path, 'w').close()
 
     with sqlite3.connect(database_path) as connection:
         func(connection)
 
-def create_db(app):
+def create_db():
     def func(connection):
         c = connection.cursor()
         c.execute('create table if not exists torrents (name character varying primary key, size integer, created_at timestamp default current_timestamp)')
         c.execute('create table if not exists downloads (id integer primary key, name character varying, size integer, created_at timestamp default current_timestamp)')
-    with_db(app, func)
+    with_db(func)
